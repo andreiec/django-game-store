dd51bd4a4b1dd235c0d37cdc36ea5eba164c5429
Andrei Constantinescu <andreiec112@gmail.com>
2021-12-25 18:51:18 +0200

    Implemented styling for all pages

--- gamestore/market/forms.py
+++ gamestore/market/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django import forms
 from .models import Game
 
 
@@ -7,3 +8,13 @@ class GameForm(ModelForm):
     class Meta:
         model = Game
         fields = ['title', 'description', 'featured_image', 'price', 'website_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    # Update styling of input
+    def __init__(self, *args, **kwargs):
+        super(GameForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
